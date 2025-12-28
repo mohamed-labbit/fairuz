95096f9f8d3876cbb87c9938655707a0b335ae4a
Mohamed Labbit <mohamed.bayt.alkhalij@gmail.com>
2025-12-28 14:37:18 +0100

    stuff

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
-rm -rf build
-mkdir build
-cd build
+#!/usr/bin/env bash
+
+if [[ "$1" == "-d" ]]; then
+    rm -rf build
+    mkdir build
+fi
+
+mkdir -p build
+cd build || exit 1
+
 cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_OSX_SYSROOT=$(xcrun --show-sdk-path) ..
 make
+
 ./mylang_tests -v
